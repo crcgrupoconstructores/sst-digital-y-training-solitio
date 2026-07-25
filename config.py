331866e10785061b5e4283622394920bfb97c2c6
crcgrupoconstructores <crcgrupoconstructores@gmail.com>
2026-07-25 14:11:33 -0500

    config.py

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
 import os
+import streamlit as st
 
 # Nombre de la Base de Datos
 DB_NAME = "centro_entrenamiento_v2.db"
-# Configuración para enviar correos (SMTP)
+
+# Configuración para enviar correos (SMTP) leyendo de los Secrets de Streamlit o valores por defecto
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 587
-EMAIL_SENDER = "crcgrupoconstructores@gmail.com"  # Pon tu correo
-EMAIL_PASSWORD = "oxjoijddedefvfuf"  # Clave de aplicación que generas en Gmail
+EMAIL_SENDER = st.secrets.get("EMAIL_SENDER", "crcgrupoconstructores@gmail.com")
+EMAIL_PASSWORD = st.secrets.get("EMAIL_PASSWORD", "oxjoijddedeivfuf")
 
-# Configuración para enviar WhatsApp (Twilio / API)
-TWILIO_ACCOUNT_SID = "AC6c9066f8b9f72de1f2896b1dafca4ef0"
-TWILIO_AUTH_TOKEN = "0c258ac69aa849866ad992e5e0201050"
-TWILIO_WHATSAPP_NUMBER = "whatsapp:+14155238886"
+# Configuración para enviar WhatsApp (Twilio / API) leyendo de los Secrets de Streamlit
+TWILIO_ACCOUNT_SID = st.secrets.get("TWILIO_ACCOUNT_SID", "AC6c9066fb9f72dc1f2896b1dafca4ef0")
+TWILIO_AUTH_TOKEN = st.secrets.get("TWILIO_AUTH_TOKEN", "0c258ac69aa849866ad992e5e0201050")
+TWILIO_WHATSAPP_NUMBER = st.secrets.get("TWILIO_WHATSAPP_NUMBER", "whatsapp:+14155238886")
